9c9c1d0334e9d2aad6a635235aa24f8fc0b1d944
Sohail Bakhshi <sohailb@Sohails-MBP.modem>
2023-04-14 01:34:34 +0800

    running customer with thread

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #include <time.h>
 #include <string.h>
 
+pthread_t thread1;
+
 void r_log(int customerNo, char serviceType, int hour, int min, int sec){
 
     FILE *logFp = fopen("r_log","a");
@@ -17,12 +19,12 @@ void r_log(int customerNo, char serviceType, int hour, int min, int sec){
 
 }
 
-void getTime(int *hour, int *min, int * sec) //https://stackoverflow.com/questions/5141960/get-the-current-time-in-c
+void getTime(int *hour, int *min, int * sec) //https://stackoverflow.com/questions/5141960/get-the-current-time-in-c a cheeky little help from stack overflow
 {
-    time_t rawtime;
+    time_t timeNow;
     struct tm * timeinfo;
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
+    time(&timeNow);
+    timeinfo = localtime(&timeNow);
     *hour =timeinfo->tm_hour;
     *min = timeinfo->tm_min;
     *sec = timeinfo->tm_sec;
@@ -30,31 +32,38 @@ void getTime(int *hour, int *min, int * sec) //https://stackoverflow.com/questio
 
 }
 
-void enqueue() {
+void enqueue(int tc,int queueSize) {
     FILE *fpFile = fopen("c_file", "r");
-    FILE *fp = fopen("c_queue", "w");
+    FILE *fpQueue = fopen("c_queue", "w");
     char line[20];
     int customerNo;
     char serviceType;
     int hour,min,sec;
 
     
-    for (int i =0; i<20;i++){
-        usleep(500000);
-        fgets(line, 20, fpFile);
-        fputs(line, fp);
+    for (int i =0; i<queueSize;i++){
+        sleep(tc);
+        fgets(line, queueSize, fpFile);
+        fputs(line, fpQueue);
         sscanf(line, "%d %c", &customerNo, &serviceType);
         getTime(&hour,&min,&sec);
         r_log(customerNo,serviceType,hour,min,sec);
     
     }
 
-    fclose(fp);
+    fclose(fpQueue);
     fclose(fpFile);
   
 }
 
-void * customer(){
+typedef struct customerArgs{
+    int *tc;
+    int size;
+}args;
+
+void * customer(void* arg){
+    args * data = arg;
+    enqueue(*data->tc,data->size);
 
     return NULL;
 }
@@ -63,8 +72,15 @@ void * customer(){
 
 
 int main(int argc, char *argv[]){
+    int tc =1;
+    int queueSize = 5;
+    args data = {&tc,queueSize};
 
-    enqueue();
+    pthread_create(&thread1,NULL,customer,&data);
+    pthread_join(thread1,NULL);
+
+
+
 
 
 
