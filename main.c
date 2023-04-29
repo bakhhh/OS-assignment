6c967d05d946a7d3690f970638457e65f9691096
Sohail Bakhshi <sohailb@Sohails-MBP.modem>
2023-04-29 12:29:14 +0800

    on task 7

--- main.c
+++ main.c
@@ -2,27 +2,17 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "linkedlist.h"
 
-pthread_t customerthread;
-pthread_t teller1;
-pthread_t teller2;
-pthread_t teller3;
-pthread_t teller4;
+
 pthread_mutex_t tellerMutex;
 pthread_cond_t tellerCond;
-
-void r_log(int customerNo, char serviceType, int hour, int min, int sec){
-
-    FILE *logFp = fopen("r_log","a");
-    fprintf(logFp,"-----------------------------------------------------------------------\n");
-    fprintf(logFp,"%d: %c\n",customerNo,serviceType);
-    fprintf(logFp, "Arrival time: %02d:%02d:%02d\n", hour, min ,sec);
-    fprintf(logFp,"-----------------------------------------------------------------------\n");
-    fclose(logFp);
-
-}
+// int hour, min, sec;
+// int startHour, startMin, startSec;
+int tw,td,ti;
+int terminateTellers= 0;
 
 
 void getTime(int *hour, int *min, int * sec) //https://stackoverflow.com/questions/5141960/get-the-current-time-in-c a cheeky little help from stack overflow
@@ -38,17 +28,44 @@ void getTime(int *hour, int *min, int * sec) //https://stackoverflow.com/questio
 
 }
 
+void resetLog() // resets the log file every time i run the program because it was annoying constantly manually deleting it
+{
+    FILE *logFp = fopen("r_log", "w");
+    if (logFp == NULL) {
+        printf("Error");
+    }
+    fclose(logFp);
+}
+
+void r_log(int customerNo, char serviceType, int hour, int min, int sec){
+
+    FILE *logFp = fopen("r_log","a");
+    fprintf(logFp,"-----------------------------------------------------------------------\n");
+    fprintf(logFp,"%d: %c\n",customerNo,serviceType);
+    fprintf(logFp, "Arrival time: %02d:%02d:%02d\n", hour, min ,sec);
+    fprintf(logFp,"-----------------------------------------------------------------------\n");
+    fclose(logFp);
+
+}
+
+
+
 typedef struct customerArgs{
     linkedlist * queue;
     int tc;
     int size;
+
 }customerArgs;
 
 typedef struct tellerArgs{
     linkedlist * queue;
-    char * tellerName;
+    int tellerNo;
     int queueSize;
+    int startHour;
+    int startMin;
+    int startSec;
 
+    
 }tellerArgs;
 
 typedef struct customerInfo{
@@ -57,73 +74,150 @@ typedef struct customerInfo{
 
 }customerInfo;
 
-void * customer(void *arg){
-    customerArgs *data = arg;
-    int hour,min,sec;
-    FILE *fpFile = fopen("c_file", "r");
-    for (int i =0; i<data->size;i++){
-        customerInfo *customers = (customerInfo*)(malloc(sizeof(customerInfo)));
-        sleep(data->tc);
-        int file_items = fscanf(fpFile, "%d %c\n", &customers->customerNo, &customers->service);
-        if (file_items != 2) {
-            free(customers);  // free memory allocated for customers
-            break; 
-        }
-        insertLast(data->queue, customers);
-        getTime(&hour, &min, &sec);
-        r_log(customers->customerNo, customers->service, hour, min, sec);
-        pthread_cond_broadcast(&tellerCond);
-    }
-    fclose(fpFile);
-  
-    return NULL;
-}
 
 void printData(void *data){
     printf("(%d, %c)->", ((customerInfo*)data)->customerNo,((customerInfo*)data)->service);
 }
 
+void * customer(void *arg){
+    int i;
+    customerArgs *data = arg;
+    int hour, min, sec;
+
+    FILE *fpFile = fopen("c_file", "r");
+    for (i =0; i<data->size;i++){
+        customerInfo *customers = (customerInfo*)(malloc(sizeof(customerInfo)));
+        sleep(data->tc);
+        int customerFile = fscanf(fpFile, "%d %c\n", &customers->customerNo, &customers->service);
+        if (customerFile != 2) { // if it reaches the end of the file then it breaks and doesnt add any more
+            free(customers);  // free memory allocated for customers
+            terminateTellers = 1;
+            pthread_cond_broadcast(&tellerCond);
+            break; 
+        }
+        insertLast(data->queue, customers);
+        getTime(&hour,&min,&sec);
+        r_log(customers->customerNo, customers->service, hour, min, sec);
+        pthread_cond_signal(&tellerCond);
+    }
+    fclose(fpFile);
+    
+  
+    return NULL;
+}
+
+
+
+
 void teller(void *arg){
     tellerArgs * data = arg;
     customerInfo * customer;
+    int hour, min, sec;
+
+    tw = 1;
+    td =1;
+    ti =1;
+
     pthread_mutex_lock(&tellerMutex);
     while(1){
+        if (terminateTellers==1 && data->queue->count ==0){
+            printf("Terminating teller %d\n",data->tellerNo);
+            pthread_cond_broadcast(&tellerCond);
+            pthread_mutex_unlock(&tellerMutex);
+            FILE *logFp = fopen("r_log","a");
+            fprintf(logFp,"------------------------------\n");
+            fprintf(logFp,"Termination: Teller-%d\n",data->tellerNo);
+            fprintf(logFp,"Served Customer: %d\n",customer->customerNo);
+            fprintf(logFp, "Start time: %02d:%02d:%02d\n", data->startHour, data->startMin ,data->startSec);
+            getTime(&hour, &min, &sec);
+            fprintf(logFp, "Termination time: %02d:%02d:%02d\n", hour, min ,sec);
+
+            break;
+        }
     
-        while(data->queue->count ==0){
-            // printf("Waiting for customer...\n");
+        while(data->queue->count ==0 && terminateTellers ==0){
+         
+            printf("teller %d Waiting..\n",data->tellerNo);
             pthread_cond_wait(&tellerCond, &tellerMutex);
+    
         }
 
-        while(data->queue->count == data->queueSize){
-            printf("Queue is full...\n");
-            pthread_cond_wait(&tellerCond, &tellerMutex);
-        }
+        // while(data->queue->count == data->queueSize){
+        //     printf("Queue is full...\n");
+        //     pthread_cond_wait(&tellerCond, &tellerMutex);
+        // }
 
-        if(data->queue->count >0){          
+        if(data->queue->count >0){  
+
             customer = deleteFirst(data->queue);
             pthread_mutex_unlock(&tellerMutex);
-            printf("%s Serving: Customer %d\n",data->tellerName,customer->customerNo);
+            FILE *logFp = fopen("r_log","a");
+            fprintf(logFp,"Teller: %d\n",data->tellerNo);
+            fprintf(logFp,"Customer: %d\n",customer->customerNo);
+            getTime(&hour,&min,&sec);
+            fprintf(logFp, "Arrival time: %02d:%02d:%02d\n", hour, min ,sec);
+            fprintf(logFp, "Response time: %02d:%02d:%02d\n", hour, min ,sec);
+            printf("Teller %d Serving: Customer %d\n",data->tellerNo,customer->customerNo);
+            
+            if (strcmp(&customer->service,"W")==0){
+                fprintf(logFp,"    Teller: %d\n",data->tellerNo);
+                fprintf(logFp,"    Customer: %d\n",customer->customerNo);
+                fprintf(logFp,"    Arrival time: %02d:%02d:%02d\n", hour, min ,sec);
+                sleep(tw);
+                getTime(&hour,&min,&sec);
+                fprintf(logFp,"    Completion time: %02d:%02d:%02d\n", hour, min ,sec);
+
+            }
+            else if (strcmp(&customer->service,"I")==0){
+                fprintf(logFp,"    Teller: %d\n",data->tellerNo);
+                fprintf(logFp,"    Customer: %d\n",customer->customerNo);
+                fprintf(logFp,"    Arrival time: %02d:%02d:%02d\n", hour, min ,sec);
+                sleep(ti);
+                getTime(&hour,&min,&sec);
+                fprintf(logFp,"    Completion time: %02d:%02d:%02d\n", hour, min ,sec);
+
+            
+            }
+            else if (strcmp(&customer->service,"D")==0){
+                fprintf(logFp,"    Teller: %d\n",data->tellerNo);
+                fprintf(logFp,"    Customer: %d\n",customer->customerNo);
+                fprintf(logFp,"    Arrival time: %02d:%02d:%02d\n", hour, min ,sec);
+                sleep(td);
+                getTime(&hour,&min,&sec);
+                fprintf(logFp,"    Completion time: %02d:%02d:%02d\n", hour, min ,sec);
+
+            }
+            fclose(logFp);
         
             }
-        else{
-            pthread_mutex_unlock(&tellerMutex);
-        }
+
+
     }
 
 
 }
 
 
-
 int main(int argc, char *argv[]){
     int tc =1;
-    int queueSize = 10;
+    int queueSize = 25;
+    int startHour, startMin, startSec;
+
+    
+    resetLog(); //reset the log file each time i run the program
+    pthread_t customerthread;
+    pthread_t teller1;
+    pthread_t teller2;
+    pthread_t teller3;
+    pthread_t teller4;
     linkedlist * queue = createLinkedList();
     customerArgs data = {queue,tc,queueSize};
-    tellerArgs teller1Args = {queue, "Teller 1",queueSize};
-    tellerArgs teller2Args = {queue, "Teller 2",queueSize};
-    tellerArgs teller3Args = {queue, "Teller 3",queueSize};
-    tellerArgs teller4Args = {queue, "Teller 4",queueSize};
+    getTime(&startHour, &startMin, &startSec);
+
+    tellerArgs teller1Args = {queue, 1 ,queueSize,startHour,startMin,startSec};
+    tellerArgs teller2Args = {queue, 2,queueSize,startHour,startMin,startSec};
+    tellerArgs teller3Args = {queue, 3,queueSize,startHour,startMin,startSec};
+    tellerArgs teller4Args = {queue, 4,queueSize,startHour,startMin,startSec};
     pthread_mutex_init(&tellerMutex, NULL);
     pthread_cond_init(&tellerCond, NULL);
     
@@ -133,6 +227,7 @@ int main(int argc, char *argv[]){
     pthread_create(&teller2,NULL,(void*)teller,(void*)&teller2Args);
     pthread_create(&teller3,NULL,(void*)teller,(void*)&teller3Args);
     pthread_create(&teller4,NULL,(void*)teller,(void*)&teller4Args);
+    
     pthread_join(customerthread,NULL);
 
     pthread_join(teller1,NULL);
@@ -143,14 +238,6 @@ int main(int argc, char *argv[]){
     freeLinkedList(queue);
     pthread_mutex_destroy(&tellerMutex);
     pthread_cond_destroy(&tellerCond);
-
-
-
-
-
-
-
-
 
 
 
